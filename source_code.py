3be3c61b8b088216a843bd4f0a7e078463ed0556
MisaghMomeniB <jenabemisagh@gmail.com>
2024-11-03 13:19:32 +0330

    Global variable to manage cancellation of the conversion process

--- source_code.py
+++ source_code.py
@@ -3,3 +3,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 import os
 import threading
+
+# Global variable to manage cancellation of the conversion process
+conversion_thread = None
+cancel_flag = False
